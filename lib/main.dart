3620f1205da9ed1c17ac3713810a9cf299d54164
ShubhamDalvi1999 <dshubhamp1999@gmail.com>
2022-03-05 17:18:33 +0530

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -22,46 +22,66 @@ class _MyAppState extends State<MyApp> {
         title: Text("Basic Flutter"),
         backgroundColor: Colors.orangeAccent,
       ),
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            children: [
-              Text("Please Login"),
-              Row(
-                children: [
-                  Text("Username"),
-                  // TextField(
-                  //   controller: _user,
-                  // ) //cannot do thiss
-                  Expanded(
-                      child: TextField(
-                    controller: _user,
-                  ))
-                ],
-              ),
-              Row(
-                children: [
-                  Text("Password"),
-                  // TextField(
-                  //   controller: _user,
-                  // ) //cannot do thiss
-                  Expanded(
-                      child: TextField(
-                    controller: _pass,
-                    obscureText: true,
-                  ))
-                ],
-              ),
-              Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: ElevatedButton(
-                    onPressed: () => {},
-                    child: Text("Login"),
-                  )),
-            ],
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50,
           ),
-        ),
+          Card(
+            child: Center(
+              child: Column(
+                children: [
+                  Text("Please Login"),
+                  Row(
+                    children: [
+                      Text("Username"),
+                      // TextField(
+                      //   controller: _user,
+                      // ) //cannot do thiss
+                      Expanded(
+                          child: TextField(
+                        controller: _user,
+                      ))
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("Password"),
+                      // TextField(
+                      //   controller: _user,
+                      // ) //cannot do thiss
+                      Expanded(
+                          child: TextField(
+                        controller: _pass,
+                        obscureText: true,
+                      ))
+                    ],
+                  ),
+                  Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: ElevatedButton(
+                        onPressed: () => {},
+                        child: Text("Login"),
+                      )),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Card(
+            color: Colors.yellow,
+            child: Center(
+              child: Column(
+                children: [
+                  Text("Hello Card"),
+                  Text("Hello Card"),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
