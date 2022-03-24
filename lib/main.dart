96e1476e7efc861cd1994d7078e641bd60b04014
ShubhamDalvi1999 <dshubhamp1999@gmail.com>
2022-03-24 17:31:12 +0530

    stepper done

--- lib/main.dart
+++ lib/main.dart
@@ -11,66 +11,71 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-//the Custom Class that we want map with a widget to display on the main body
-class Choice {
-  final String title;
-  final IconData icon;
-
-  Choice({
-    required this.title,
-    required this.icon,
-  });
-}
-
-//with TickerProviderStateMixin is a fix to the error of "vsync: this"
-
-class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
-  late TabController _controller;
-  List<Choice> _items = <Choice>[
-    Choice(title: "Car", icon: Icons.directions_car),
-    Choice(title: "Bicycle", icon: Icons.directions_bike),
-  ];
+class _MyAppState extends State<MyApp> {
+  late int _current;
+  late List<Step> _items;
 
   @override
   void initState() {
     super.initState();
-    _controller = new TabController(length: _items.length, vsync: this);
+    _current = 0;
+    _items = [
+      Step(
+          title: Text("Step 1"),
+          content: Text("Do Something *0(doesn't even matter)"),
+          isActive: true),
+      Step(
+          title: Text("Step 2"),
+          content: Text("Do Something *0(doesn't even matter)"),
+          isActive: true),
+      Step(
+          title: Text("Step 3"),
+          content: Text("Do Something *0(doesn't even matter)"),
+          isActive: true)
+    ];
+  }
+
+  void _stepContinue() {
+    setState(() {
+      _current++;
+      if (_current >= _items.length) {
+        _current = _items.length - 1;
+      }
+    });
+  }
+
+  void _stepCancel() {
+    setState(() {
+      _current--;
+      if (_current < 0) {
+        _current = 0;
+      }
+    });
+  }
+
+  void _stepTap(int index) {
+    setState(() {
+      _current = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("App2"),
-          bottom: PreferredSize(
-              child: Theme(
-                  data: Theme.of(context).copyWith(
-                      colorScheme: ColorScheme.fromSwatch()
-                          .copyWith(secondary: Colors.white)),
-                  child: Container(
-                    alignment: Alignment.center,
-                    child: TabPageSelector(
-                      controller: _controller,
-                    ), //TabPageSelector controller
-                  )),
-              preferredSize: Size.fromHeight(48.0)),
+          title: Text("App"),
         ),
-        body: TabBarView(
-          controller: _controller,
-          //mapping object of each class to a WIDGET
-          children: _items
-              .map((Choice i) => Container(
-                    padding: EdgeInsets.all(32.0),
-                    child: Center(
-                      child: Column(
-                        children: [
-                          Text(i.title),
-                          Icon(i.icon, size: 120.0),
-                        ],
-                      ),
-                    ),
-                  ))
-              .toList(),
+        body: Container(
+          padding: EdgeInsets.all(32.0),
+          child: Center(
+              child: Stepper(
+            steps: _items,
+            type: StepperType.vertical,
+            currentStep: _current,
+            onStepCancel: _stepCancel,
+            onStepContinue: _stepContinue,
+            onStepTapped: _stepTap,
+          )),
         ));
   }
 }
