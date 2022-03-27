1d4404204b951cfb2102082f27ab8b6f09fd73ec
ShubhamDalvi1999 <dshubhamp1999@gmail.com>
2022-03-27 16:51:18 +0530

    GridView Game

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,71 +13,74 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+class Area {
+  int index;
+  String name;
+  Color color;
+  Area({
+    this.index: -1,
+    this.name: "Area",
+    this.color: Colors.lightBlueAccent,
+  });
+}
+
 class _MyAppState extends State<MyApp> {
-  late int _current;
-  late List<Step> _items;
+  List<Area> _areas = [];
+  int _location = 0;
 
   @override
   void initState() {
     super.initState();
-    _current = 0;
-    _items = [
-      Step(
-          title: Text("Step 1"),
-          content: Text("Do Something *0(doesn't even matter)"),
-          isActive: true),
-      Step(
-          title: Text("Step 2"),
-          content: Text("Do Something *0(doesn't even matter)"),
-          isActive: true),
-      Step(
-          title: Text("Step 3"),
-          content: Text("Do Something *0(doesn't even matter)"),
-          isActive: true)
-    ];
+    for (var i = 0; i < 16; i++) {
+      _areas.add(Area(index: i, name: "Area $i"));
+    }
+    var rndm = new Random();
+    _location = rndm.nextInt(_areas.length);
   }
 
-  void _stepContinue() {
+  void _onPressed(int index) {
+    print("location is $_location");
+    print("index clicked is $index");
     setState(() {
-      _current++;
-      if (_current >= _items.length) {
-        _current = _items.length - 1;
+      if (index == _location) {
+        _areas[index].color = Colors.lightGreen;
+      } else {
+        _areas[index].color = Colors.red;
       }
     });
   }
 
-  void _stepCancel() {
-    setState(() {
-      _current--;
-      if (_current < 0) {
-        _current = 0;
-      }
-    });
-  }
-
-  void _stepTap(int index) {
-    setState(() {
-      _current = index;
-    });
+  Widget _generate(int index) {
+    return GridTile(
+        child: Container(
+      padding: EdgeInsets.all(5.0),
+      child: ElevatedButton(
+          onPressed: () => _onPressed(index),
+          //color property
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(_areas[index].color),
+          ),
+          child: Text(
+            _areas[index].name,
+            textAlign: TextAlign.center,
+          )),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("App"),
-        ),
-        body: Container(
+      appBar: AppBar(
+        title: Text("App"),
+      ),
+      body: Container(
           padding: EdgeInsets.all(32.0),
           child: Center(
-              child: Stepper(
-            steps: _items,
-            type: StepperType.vertical,
-            currentStep: _current,
-            onStepCancel: _stepCancel,
-            onStepContinue: _stepContinue,
-            onStepTapped: _stepTap,
+            child: GridView.count(
+              crossAxisCount: 4,
+              children: List.generate(16, (index) => _generate(index)),
+            ),
           )),
-        ));
+    );
   }
 }
